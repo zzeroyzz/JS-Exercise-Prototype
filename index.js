a077acc5417b4416947ed73c35eacc12f7f476ef
kato hawkins <kristhahawkins@icloud.com>
2020-06-11 15:37:12 -0400

    assignment complete

--- index.js
+++ index.js
@@ -39,9 +39,24 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+this.name = name;
+this.age = age;
+this.stomach = [];
 }
+
+Person.prototype.eat = function(foodies) {
+  if (this.stomach.length < 10) 
+  this.stomach.push(foodies);
+};
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+};
 
 /*
   TASK 2
@@ -57,9 +72,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons) {
+  return (this.tank += gallons);
+};
+Car.prototype.drive = function(distance) {
+  this.odometer += distance;
+  this.tank -= distance / this.milesPerGallon;
+  if (this.tank <= 0) {
+    this.odometer += this.tank * this.milesPerGallon; 
+    this.tank = 0; 
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+};
 
 /*
   TASK 3
@@ -68,18 +99,22 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`;
+};
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. In the global scope, values are referred as the global object
+  2. Whenever a preceding dot calls a function, the object before the dot is this.
+  3. This is explicit when it is being used in a call or apply method
+  4. Whenever we use a constructor function, this refers to the specific instance of the object that is created and returned by the constructor function.
 */
 
 
